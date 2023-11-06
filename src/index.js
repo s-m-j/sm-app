109417eff267385a81e5672a4f36a3e7ff94f55b
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-11-06 21:10:12 +0300

    Вложенность элементов React

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ import ReactDOM from "react-dom/client";
 } */
 
 function Greeting() {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Hello from My Component")
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
 }
+
+const Person = () => <h2>Vasya Pupkin</h2>;
+const Message = () => {
+  return <p>Simple message</p>;
+};
 
 // В шаблоне документа (index.html) находим элемент с идентификатором root
 // И сопоставляем с ним корневой элемент приложения (этот элемент единственный, остальные будут вложены в нем)
