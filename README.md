6c34925c7b0fcbdbabd4caf0bb6f85c5b080b37a
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-11-06 18:11:39 +0300

    Первая рабочая версия. Создали свой корневой эл-т

--- README.md
+++ README.md
@@ -16,3 +16,26 @@
 - package.json - каждое приложение node.js содержит файл с описанием метаданных приложения (в частности, зависимости приложения, скрипты которые оно выполняет)
 
 - package-lock.json - слепок дерева зависимостей
+
+### Создание приложения
+
+Сначала удалим папку src которую создал за нас create-react-app. Теперь создадим папку с таким же именем и поместим в нее
+файл (пустой) index.js
+
+В файле сохраняем такой код
+
+import React from "react";
+import ReactDOM from "react=dom/client";
+
+function Greeting() {
+return <h3>My First Componene</h3>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
+
+и видим что наше приложение запустилось. То есть, мы создали функцию (можно было и "стрелочную") которая:
+
+- Начинается с заглавной буквы
+- возвращает разметку JSX
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react=dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return <h3>My First Componene</h3>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// В шаблоне документа (index.html) находим элемент с идентификатором root
+// И сопоставляем с ним корневой элемент приложения (этот элемент единственный, остальные будут вложены в нем)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
